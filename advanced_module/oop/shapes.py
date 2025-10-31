bf41a9fb65d44d0fd40b1c407c5b7c2a18b96bb0
Viksata <viki1204394@gmail.com>
2025-10-31 15:58:09 +0200

    OOP Module Practice

---
+++ advanced_module/oop/shapes.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shapes(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shapes):
+
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius**2
+
+    def calculate_perimeter(self):
+        return 2 * pi * self.__radius
+
+
+class Rectangle(Shapes):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_perimeter(self):
+        return 2 * (self.__width + self.__height)
+
+    def calculate_area(self):
+        return self.__width * self.__height
+
+
+rectangle = Rectangle(10, 20)
+
+print(rectangle.calculate_area())
+
+print(rectangle.calculate_perimeter())
