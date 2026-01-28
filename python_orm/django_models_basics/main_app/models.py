13150ff05222efe2a77bc9bb0ecafd865ec0e3ac
Viksata <viki1204394@gmail.com>
2026-01-28 18:47:16 +0200

    Created Department model

--- python_orm/django_models_basics/main_app/models.py
+++ python_orm/django_models_basics/main_app/models.py
@@ -8,3 +8,16 @@ class Employee(models.Model):
     birth_date = models.DateField()
     works_full_time = models.BooleanField()
     created_on = models.DateTimeField(auto_now_add=True)
+
+class Department(models.Model):
+    class Locations(models.TextChoices):
+        SOFIA = 'Sofia', 'Sofia'
+        PLOVDIV = 'Plovdiv', 'Plovdiv'
+        BURGAS = 'Burgas', 'Burgas'
+        VARNA = 'Varna', 'Varna'
+
+    code = models.CharField(max_length=4, primary_key=True, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    employee_count = models.PositiveIntegerField(default=1, verbose_name="Employees Count")
+    location = models.CharField(max_length=20, choices=Locations, blank=True, null=True)
+    last_edited_on = models.DateTimeField(auto_now=True, editable=False)
