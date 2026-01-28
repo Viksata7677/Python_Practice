d729445f9d0117e1505bb5863973a655da481cae
Viksata <viki1204394@gmail.com>
2026-01-28 19:24:54 +0200

    Fixed model fields

--- python_orm/django_models_basics/main_app/models.py
+++ python_orm/django_models_basics/main_app/models.py
@@ -18,6 +18,6 @@ class Department(models.Model):
 
     code = models.CharField(max_length=4, primary_key=True, unique=True)
     name = models.CharField(max_length=50, unique=True)
-    employee_count = models.PositiveIntegerField(default=1, verbose_name="Employees Count")
-    location = models.CharField(max_length=20, choices=Locations, blank=True, null=True)
+    employees_count = models.PositiveIntegerField(default=1, verbose_name="Employees Count")
+    location = models.CharField(max_length=20, choices=Locations)
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
