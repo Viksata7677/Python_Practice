d601ed3ce6cc0e675a2f886b430f8006f7ecdc46
Viksata <viki1204394@gmail.com>
2026-01-28 19:40:04 +0200

    Created Project model

--- python_orm/django_models_basics/main_app/models.py
+++ python_orm/django_models_basics/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
@@ -20,4 +22,14 @@ class Department(models.Model):
     name = models.CharField(max_length=50, unique=True)
     employees_count = models.PositiveIntegerField(default=1, verbose_name="Employees Count")
     location = models.CharField(max_length=20, choices=Locations)
+    last_edited_on = models.DateTimeField(auto_now=True, editable=False)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+    budget = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    duration_in_days = models.PositiveIntegerField(null=True, blank=True, verbose_name="Duration in Days")
+    estimated_hours = models.FloatField(null=True, blank=True, verbose_name="Estimated Hours")
+    start_date = models.DateField(verbose_name="Start Date", null=True, blank=True, default=date.today())
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
