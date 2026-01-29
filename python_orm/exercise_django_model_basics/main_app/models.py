b2f6efff4ab39ed7f86c1f7fb50b613cd132fabe
Viksata <viki1204394@gmail.com>
2026-01-29 20:28:13 +0200

    Alter max_length on name field

--- python_orm/exercise_django_model_basics/main_app/models.py
+++ python_orm/exercise_django_model_basics/main_app/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
