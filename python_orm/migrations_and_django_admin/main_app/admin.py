c5d5dd5acc644fa3a9c7492cd49a65b0c6873bd1
Viksata <viki1204394@gmail.com>
2026-02-10 18:04:57 +0200

    Customized admin panel

--- python_orm/migrations_and_django_admin/main_app/admin.py
+++ python_orm/migrations_and_django_admin/main_app/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from unicodedata import category
 
 from main_app.models import Product
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'created_on']
+    list_filter = ['category', 'supplier']
+    search_fields = ['category', 'supplier']
+    fieldsets = [
+        ('General Information', {
+            'fields': [
+                'name', 'description', 'price', 'barcode'
+            ],
+        }),
+        ('Categorization', {
+            'fields': [
+                'category', 'supplier'
+            ],
+        }),
+    ]
+    date_hierarchy = 'created_on'
