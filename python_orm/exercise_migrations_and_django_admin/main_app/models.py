3ce35fd47d6c741c0034f21e5fa0d20791fd307e
Viksata <viki1204394@gmail.com>
2026-02-13 14:35:40 +0200

    Created Course model

--- python_orm/exercise_migrations_and_django_admin/main_app/models.py
+++ python_orm/exercise_migrations_and_django_admin/main_app/models.py
@@ -51,3 +51,15 @@ class Supplier(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.phone}"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=90)
+    lecturer = models.CharField(max_length=90)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    start_date = models.DateField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.lecturer}"
