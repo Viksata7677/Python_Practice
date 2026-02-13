1e9d51193bedf3bba774f92cdbd89e9c59810067
Viksata <viki1204394@gmail.com>
2026-02-13 14:31:43 +0200

    Fixed contact_person's max_length

--- python_orm/exercise_migrations_and_django_admin/main_app/models.py
+++ python_orm/exercise_migrations_and_django_admin/main_app/models.py
@@ -44,7 +44,7 @@ class Student(models.Model):
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
-    contact_person = models.CharField(max_length=100)
+    contact_person = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, unique=True)
     address = models.TextField()
