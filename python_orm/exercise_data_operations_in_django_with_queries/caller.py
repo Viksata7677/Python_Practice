d74ff464615ba6ec2182681e7da966f6fdacb083
Viksata <viki1204394@gmail.com>
2026-02-15 20:10:36 +0200

    Created Artifact model

--- python_orm/exercise_data_operations_in_django_with_queries/caller.py
+++ python_orm/exercise_data_operations_in_django_with_queries/caller.py
@@ -6,7 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Pet
+from main_app.models import Pet, Artifact
+
 
 # Create queries within functions
 
@@ -14,4 +15,27 @@ from main_app.models import Pet
 def create_pet(name: str, species: str):
     pet = Pet.objects.create(name=name, species=species)
     return f"{pet.name} is a very cute {pet.species}!"
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+    artifact = Artifact()
+    artifact.name = name
+    artifact.origin = origin
+    artifact.age = age
+    artifact.description = description
+    artifact.is_magical = is_magical
+
+    artifact.save()
+
+    return f"The artifact {artifact.name} is {artifact.age} years old!"
+
+
+def rename_artifact(artifact: Artifact, new_name: str):
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+        artifact.save()
+
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
 
