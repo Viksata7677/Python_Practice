c9238e40b1be995582e8af54a1cdec3bdfecfe1c
Viksata <viki1204394@gmail.com>
2026-02-17 16:38:59 +0200

    Added functions for ChessPlayer model

--- python_orm/exercise_working_with_queries_in_django/caller.py
+++ python_orm/exercise_working_with_queries_in_django/caller.py
@@ -9,7 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import ArtworkGallery, Laptop
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer
 
 
 # Create and check models
@@ -53,4 +53,40 @@ def update_operation_systems():
 
 def delete_inexpensive_laptops():
     Laptop.objects.filter(price__lt=1200).delete()
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]):
+    ChessPlayer.objects.bulk_create(args)
+
+
+def delete_chess_players():
+    ChessPlayer.objects.filter(title='no title').delete()
+
+
+def change_chess_games_won():
+    ChessPlayer.objects.filter(title='GM').update(games_won = 30)
+
+
+def change_chess_games_lost():
+    ChessPlayer.objects.filter(title='no title').update(games_lost = 25)
+
+
+def change_chess_games_drawn():
+    ChessPlayer.objects.all().update(games_drawn = 10)
+
+
+def grand_chess_title_GM():
+    ChessPlayer.objects.filter(rating__gte=2400).update(title = 'GM')
+
+
+def grand_chess_title_IM():
+    ChessPlayer.objects.filter(rating__gte=2300, rating__lte=2399).update(title='IM')
+
+
+def grand_chess_title_FM():
+    ChessPlayer.objects.filter(rating__gte=2200, rating__lte=2299).update(title='FM')
+
+
+def grand_chess_title_regular_player():
+    ChessPlayer.objects.filter(rating__gte=0, rating__lte=2199).update(title='regular player')
 # Run and print your queries
