990aaf8d7dd253f6e0ee112118a5911864b44878
Viksata <viki1204394@gmail.com>
2026-02-25 18:23:37 +0200

    Created model with custom validator

--- python_orm/exercises_advanced_django_model_techniques/caller.py
+++ python_orm/exercises_advanced_django_model_techniques/caller.py
@@ -4,7 +4,3 @@ import django
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
-
-# Import your models here
-
-# Create queries within functions
