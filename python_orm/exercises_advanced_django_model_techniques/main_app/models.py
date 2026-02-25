169756b9269fb2b66e6a2c73bc25cd5c8f09099f
Viksata <viki1204394@gmail.com>
2026-02-25 19:26:02 +0200

    Created Product model

--- python_orm/exercises_advanced_django_model_techniques/main_app/models.py
+++ python_orm/exercises_advanced_django_model_techniques/main_app/models.py
@@ -24,6 +24,7 @@ class BaseMedia(models.Model):
         abstract = True
         ordering = ['-created_at', 'title']
 
+
 class Book(BaseMedia):
     author = models.CharField(max_length=100, validators=[MinLengthValidator(5, message="Author must be at least 5 characters long")])
     isbn = models.CharField(max_length=20, unique=True, validators=[MinLengthValidator(6, message="ISBN must be at least 6 characters long")])
@@ -32,6 +33,7 @@ class Book(BaseMedia):
         verbose_name = 'Model Book'
         verbose_name_plural = 'Models of type - Book'
 
+
 class Movie(BaseMedia):
     director = models.CharField(max_length=100, validators=[MinLengthValidator(8, message="Director must be at least 8 characters long")])
 
@@ -39,9 +41,15 @@ class Movie(BaseMedia):
         verbose_name = 'Model Movie'
         verbose_name_plural = 'Models of type - Movie'
 
+
 class Music(BaseMedia):
     artist = models.CharField(max_length=100, validators=[MinLengthValidator(9, message="Artist must be at least 9 characters long")])
 
     class Meta(BaseMedia.Meta):
         verbose_name = 'Model Music'
         verbose_name_plural = 'Models of type - Music'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
