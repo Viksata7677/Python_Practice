35d2e17703426d59b753db2de84fe70ca41bce71
Viksata <viki1204394@gmail.com>
2026-02-25 19:22:36 +0200

    Added functionality to class Meta

--- python_orm/exercises_advanced_django_model_techniques/main_app/models.py
+++ python_orm/exercises_advanced_django_model_techniques/main_app/models.py
@@ -22,16 +22,26 @@ class BaseMedia(models.Model):
 
     class Meta:
         abstract = True
-
+        ordering = ['-created_at', 'title']
 
 class Book(BaseMedia):
     author = models.CharField(max_length=100, validators=[MinLengthValidator(5, message="Author must be at least 5 characters long")])
     isbn = models.CharField(max_length=20, unique=True, validators=[MinLengthValidator(6, message="ISBN must be at least 6 characters long")])
 
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Book'
+        verbose_name_plural = 'Models of type - Book'
 
 class Movie(BaseMedia):
     director = models.CharField(max_length=100, validators=[MinLengthValidator(8, message="Director must be at least 8 characters long")])
 
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Movie'
+        verbose_name_plural = 'Models of type - Movie'
 
 class Music(BaseMedia):
-    artist = models.CharField(max_length=100, validators=[MinLengthValidator(9, message="Artist must be at least 9 characters long".)])
+    artist = models.CharField(max_length=100, validators=[MinLengthValidator(9, message="Artist must be at least 9 characters long")])
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = 'Model Music'
+        verbose_name_plural = 'Models of type - Music'
