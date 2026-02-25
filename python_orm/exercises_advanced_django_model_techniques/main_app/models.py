1f447ad710610370fd329a32d30e74699d4ec8d1
Viksata <viki1204394@gmail.com>
2026-02-25 19:15:11 +0200

    Created Movie model

--- python_orm/exercises_advanced_django_model_techniques/main_app/models.py
+++ python_orm/exercises_advanced_django_model_techniques/main_app/models.py
@@ -27,3 +27,7 @@ class BaseMedia(models.Model):
 class Book(BaseMedia):
     author = models.CharField(max_length=100, validators=[MinLengthValidator(5, message="Author must be at least 5 characters long")])
     isbn = models.CharField(max_length=20, unique=True, validators=[MinLengthValidator(6, message="ISBN must be at least 6 characters long")])
+
+
+class Movie(BaseMedia):
+    director = models.CharField(max_length=100, validators=[MinLengthValidator(8, message="Director must be at least 8 characters long")])
