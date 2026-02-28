23f85154f47b7892a5bba4b683267f68bd9c3dab
Viksata <viki1204394@gmail.com>
2026-02-28 20:50:22 +0200

    Created custom manager

--- python_orm/advanced_queries_in_django/main_app/models.py
+++ python_orm/advanced_queries_in_django/main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class ProductManager(models.Manager):
+    def available_products(self):
+        return self.filter(is_available=True)
+
+    def available_products_in_category(self, category_name: str):
+        return self.filter(category__name=category_name, is_available=True)
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -11,6 +18,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return f"{self.category.name}: {self.name}"
