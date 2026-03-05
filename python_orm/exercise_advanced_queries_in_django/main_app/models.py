76f6c027f7147fb488041cc610446c9cde4c85a3
Viksata <viki1204394@gmail.com>
2026-03-05 14:50:00 +0200

    Added 4 class methods to Task model

--- python_orm/exercise_advanced_queries_in_django/main_app/models.py
+++ python_orm/exercise_advanced_queries_in_django/main_app/models.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from decimal import Decimal
 from django.db import models
+from django.db.models import F, Q
 from django.db.models.aggregates import Count, Max, Avg
 
 from main_app.validators import RangeValidator
@@ -131,6 +133,22 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     creation_date = models.DateField()
     completion_date = models.DateField()
+
+    @classmethod
+    def ongoing_high_priority_tasks(cls):
+        return cls.objects.filter(priority='High', is_completed=False, completion_date__gt=F('creation_date'))
+
+    @classmethod
+    def completed_mid_priority_tasks(cls):
+        return cls.objects.filter(priority='Medium', is_completed=True)
+
+    @classmethod
+    def search_tasks(cls, query: str):
+        return cls.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    @classmethod
+    def recent_completed_tasks(cls, days: int):
+        return cls.objects.filter(is_completed=True, completion_date__gte=F('creation_date') - timedelta(days=days))
 
 
 class Exercise(models.Model):
