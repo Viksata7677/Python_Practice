8a21c642244027e3c20649fff41667ad88df5af6
Viksata <viki1204394@gmail.com>
2026-03-06 13:13:08 +0200

    Get directors by name or/and nationality

--- python_orm/exam_preparation/caller.py
+++ python_orm/exam_preparation/caller.py
@@ -1,10 +1,37 @@
 import os
 import django
 
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
 
+from main_app.models import Director
+from django.db.models import Q
+
+def get_directors(search_name=None, search_nationality=None):
+    if search_name is None and search_nationality is None:
+        return ''
+
+    query_name = Q(full_name__icontains=search_name)
+    query_nationality = Q(nationality__icontains=search_nationality)
+
+    if search_name is not None and search_nationality is not None:
+        query = Q(query_name & query_nationality)
+    elif search_name is not None:
+        query = query_name
+    else:
+        query = query_nationality
+
+    directors = Director.objects.filter(query).order_by('full_name')
+    if not directors:
+        return ''
+
+    result = []
+    for d in directors:
+        result.append(f'Director: {d.full_name}, nationality: {d.nationality}, experience: {d.years_of_experience}')
+
+    return '\n'.join(result)
 
