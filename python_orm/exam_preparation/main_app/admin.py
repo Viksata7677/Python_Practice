2c763fdb6f9148e736e0844512864f4d042b18f1
Viksata <viki1204394@gmail.com>
2026-03-06 12:22:31 +0200

    Registered Director model in admin

--- python_orm/exam_preparation/main_app/admin.py
+++ python_orm/exam_preparation/main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from main_app.models import Director
+
+
 # Register your models here.
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'birth_date', 'nationality']
+    list_filter = ['years_of_experience']
+    search_fields = ['full_name', 'nationality']
