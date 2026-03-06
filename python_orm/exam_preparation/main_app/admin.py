88c2079385e167485c2c24b83fead29aa3ed05dc
Viksata <viki1204394@gmail.com>
2026-03-06 12:28:27 +0200

    Register Movie model in admin site

--- python_orm/exam_preparation/main_app/admin.py
+++ python_orm/exam_preparation/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import Director, Actor
+from main_app.models import Director, Actor, Movie
 
 
 # Register your models here.
@@ -16,4 +16,12 @@ class ActorAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'birth_date', 'nationality']
     list_filter = ['is_awarded']
     search_fields = ['full_name']
+    readonly_fields = ['last_updated']
+    
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'storyline', 'rating', 'director']
+    list_filter = ['is_awarded', 'is_classic', 'genre']
+    search_fields = ['title', 'director__full_name']
     readonly_fields = ['last_updated']
