26c9ca3a41dd6e1056551d05a9f37a3ad3bb7687
Viksata <viki1204394@gmail.com>
2026-03-06 12:25:15 +0200

    Register Actor model in admin site

--- python_orm/exam_preparation/main_app/admin.py
+++ python_orm/exam_preparation/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import Director
+from main_app.models import Director, Actor
 
 
 # Register your models here.
@@ -9,3 +9,11 @@ class DirectorAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'birth_date', 'nationality']
     list_filter = ['years_of_experience']
     search_fields = ['full_name', 'nationality']
+    
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'birth_date', 'nationality']
+    list_filter = ['is_awarded']
+    search_fields = ['full_name']
+    readonly_fields = ['last_updated']
