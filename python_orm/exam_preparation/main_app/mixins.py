693b6130ad6e5b0a8ad4aa4c797fbef2a5476767
Viksata <viki1204394@gmail.com>
2026-03-06 11:59:26 +0200

    Created mixin for last_updated field

--- python_orm/exam_preparation/main_app/mixins.py
+++ python_orm/exam_preparation/main_app/mixins.py
@@ -6,3 +6,10 @@ class AwardedMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+class LastUpdatedMixin(models.Model):
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- python_orm/exam_preparation/main_app/models.py
+++ python_orm/exam_preparation/main_app/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MinLengthValidator, MinValueValidator, MaxLen
 from django.db import models
 from django.db.models import TextChoices, CASCADE
 
-from main_app.mixins import AwardedMixin
+from main_app.mixins import AwardedMixin, LastUpdatedMixin
 
 
 # Create your models here.
@@ -21,8 +21,8 @@ class Director(BaseModel):
     years_of_experience = models.SmallIntegerField(validators=[MinValueValidator(0)], default=0)
 
 
-class Actor(BaseModel, AwardedMixin):
-    last_updated = models.DateTimeField(auto_now=True)
+class Actor(BaseModel, AwardedMixin, LastUpdatedMixin):
+    pass
 
 
 class GenreChoices(TextChoices):
@@ -32,14 +32,13 @@ class GenreChoices(TextChoices):
     OTHER = 'Other', 'Other'
 
 
-class Movie(models.Model, AwardedMixin):
+class Movie(models.Model, AwardedMixin, LastUpdatedMixin):
     title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
     release_date = models.DateField()
     storyline = models.TextField(null=True, blank=True)
     genre = models.CharField(max_length=6, choices=GenreChoices.choices, default=GenreChoices.OTHER)
     rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], default=0.0)
     is_classic = models.BooleanField(default=False)
-    last_updated = models.DateTimeField(auto_now=True)
     director = models.ForeignKey(to=Director, on_delete=models.CASCADE)
     starring_actor = models.ForeignKey(to=Actor, null=True, on_delete=models.SET_NULL)
     actors = models.ManyToManyField(to=Actor)
