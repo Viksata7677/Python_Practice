ddf1d6bbf459bad7b7113bc76a1b02c16bf15ee5
Viksata <viki1204394@gmail.com>
2026-03-12 14:18:39 +0200

    Register Product model in admin site

--- python_orm/exam_prep2/main_app/admin.py
+++ python_orm/exam_prep2/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import Profile
+from main_app.models import Profile, Product
 
 
 # Register your models here.
@@ -8,3 +8,10 @@ from main_app.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'email', 'phone_number', 'is_active']
     search_fields = ['full_name', 'email']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'in_stock', 'is_available']
+    list_filter = ['is_available']
+    search_fields = ['name']
