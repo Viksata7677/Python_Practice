3369feee5b08863a244c4c5331f161c2c3fae3bc
Viksata <viki1204394@gmail.com>
2026-03-12 14:17:35 +0200

    Register Profile model in admin site

--- python_orm/exam_prep2/main_app/admin.py
+++ python_orm/exam_prep2/main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from main_app.models import Profile
+
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone_number', 'is_active']
+    search_fields = ['full_name', 'email']
