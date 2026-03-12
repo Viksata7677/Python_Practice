40350c0089b49b187db724f9b1e43e76ac47fca2
Viksata <viki1204394@gmail.com>
2026-03-12 13:48:16 +0200

    Created Product model

--- python_orm/exam_prep2/main_app/models.py
+++ python_orm/exam_prep2/main_app/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -8,4 +8,13 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.TextField()
     is_active = models.BooleanField(default=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    is_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=True)
     creation_date = models.DateTimeField(auto_now_add=True)
