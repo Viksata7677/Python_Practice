86fd4e2a6da1238869294064edfe7ddf9eac4670
Viksata <viki1204394@gmail.com>
2026-03-12 15:17:33 +0200

    Created get_last_sold_products function

--- python_orm/exam_prep2/caller.py
+++ python_orm/exam_prep2/caller.py
@@ -2,20 +2,20 @@ import os
 import django
 from django.db.models import Q
 
-from main_app.models import Profile
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Profile, Order
 
 
 def get_profiles(search_string=None):
     if search_string is None:
         return ''
 
-    profiles = Profile.objects.filter(Q(full_name__icontains=search_string) | Q(email__icontains=search_string) | Q(phone_number__icontains=search_string))
+    profiles = Profile.objects.filter(Q(full_name__icontains=search_string) | Q(email__icontains=search_string) | Q(phone_number__icontains=search_string)).order_by('full_name')
 
     if not profiles.exists():
         return ''
@@ -30,3 +30,14 @@ def get_loyal_profiles():
         return ''
 
     return '\n'.join(f"Profile: {p.full_name}, orders: {p.orders.count()}" for p in profiles)
+
+
+def get_last_sold_products():
+    last_order = Order.objects.prefetch_related('products').last()
+
+    if last_order is None or not last_order.products.exists():
+        return ''
+
+    products = ', '.join([p.name for p in last_order.products.order_by('name')])
+
+    return f"Last sold products: {products}"
