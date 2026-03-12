e7e461bf75cca4ea85c8dd742af4d88749db3919
Viksata <viki1204394@gmail.com>
2026-03-12 14:53:53 +0200

    Created get_profiles function

--- python_orm/exam_prep2/caller.py
+++ python_orm/exam_prep2/caller.py
@@ -1,9 +1,24 @@
 import os
 import django
+from django.db.models import Q
+
+from main_app.models import Profile
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+
+
+def get_profiles(search_string=None):
+    if search_string is None:
+        return ''
+
+    profiles = Profile.objects.filter(Q(full_name__icontains=search_string) | Q(email__icontains=search_string) | Q(phone_number__icontains=search_string))
+
+    if not profiles.exists():
+        return ''
+
+    return '\n'.join(f"Profile: {p.full_name}, email: {p.email}, phone number: {p.phone_number}, orders: {p.orders.count()}" for p in profiles)
 
