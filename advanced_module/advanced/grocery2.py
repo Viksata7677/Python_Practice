f0cb85a6462544b14fff22eaef4f794a7edefa85
Viksata <viki1204394@gmail.com>
2026-07-12 16:05:30 +0300

    Sort items with sorted()

---
+++ advanced_module/advanced/grocery2.py
@@ -0,0 +1,17 @@
+def grocery_store(**kwargs):
+    sorted_products = sorted(kwargs.items(), key= lambda x: (-x[1], len(x[0]), x[0]))
+    result = []
+    for product, quantity in sorted_products:
+        result.append(f"{product}: {quantity}")
+
+    return "\n".join(result)
+
+print(grocery_store(
+
+bread=5,
+
+pasta=12,
+
+eggs=12,
+
+))
