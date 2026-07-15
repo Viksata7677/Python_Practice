f406f6c459ddc91aa2442fc57db27e42a59804fb
Viksata <viki1204394@gmail.com>
2026-07-15 10:25:19 +0300

    Print the results of fishing competition

--- advanced_module/advanced/fishing_competition.py
+++ advanced_module/advanced/fishing_competition.py
@@ -41,10 +41,33 @@ for row_index in range(n):
     matrix.append(data)
 
 command = input()
+collected_fish = 0
 
 while command != 'collect the nets':
+    current_row_index, current_col_index = position
     next_row_index, next_col_index = get_next_position(position, direction_mapper, command, matrix)
 
+    symbol = matrix[next_row_index][next_col_index]
+    matrix[next_row_index][next_col_index] = 'S'
+    matrix[current_row_index][current_col_index] = '-'
+    position = [next_row_index, next_col_index]
 
+    if symbol.isdigit():
+        collected_fish += int(symbol)
+    elif symbol == 'W':
+        print(f"You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinated of the ship: {position}")
+        exit()
 
     command = input()
+
+if collected_fish >= 20:
+    print("Success! You managed to reach the quota!")
+else:
+    print(f"You didn't catch enough fish and didn't reach the quota!\n"
+          f"You need {20 - collected_fish} tons of fish more.")
+
+if collected_fish > 0:
+    print(f"Amount of fish caught: {collected_fish} tons")
+
+for row in matrix:
+    print(f"{''.join(row)}")
